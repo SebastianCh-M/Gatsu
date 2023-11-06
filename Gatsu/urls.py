7207581cf33026fe198847c0e39f4086ac3ee567
Darceu <di.arceu@duocuc.cl>
2023-11-06 00:35:48 -0300

    imagenes de prueba y carrusel

--- Gatsu/urls.py
+++ Gatsu/urls.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+
 from .views import HomeView, RecientesView, TopMangasView, SobreGatsuView
 
 urlpatterns = [
@@ -9,4 +12,4 @@ urlpatterns = [
     path('TopMangas.html', TopMangasView.as_view(), name='TopMangas'),
     path('SobreGatsu.html', SobreGatsuView.as_view(), name='SobreGatsu'),
     path('', HomeView.as_view(), name='default'),  # Ruta para la página por defecto
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
