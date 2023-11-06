cf19c1fa86145978c531f5b688c17da6f63c7907
Darceu <di.arceu@duocuc.cl>
2023-11-05 23:31:43 -0300

    Config urls, views y navbar

--- Gatsu/urls.py
+++ Gatsu/urls.py
@@ -1,13 +1,12 @@
 from django.contrib import admin
-from django.urls import path, include
-from .views import HomeView
-
-
+from django.urls import path
+from .views import HomeView, RecientesView, TopMangasView, SobreGatsuView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-
-    path('', HomeView.as_view(), name="home"),
-
-    path('manga/', include('manga.urls', namespace='manga'))
+    path('Home.html', HomeView.as_view(), name='home'),
+    path('Recientes.html', RecientesView.as_view(), name='recientes'),
+    path('TopMangas.html', TopMangasView.as_view(), name='TopMangas'),
+    path('SobreGatsu.html', SobreGatsuView.as_view(), name='SobreGatsu'),
+    path('', HomeView.as_view(), name='default'),  # Ruta para la página por defecto
 ]
