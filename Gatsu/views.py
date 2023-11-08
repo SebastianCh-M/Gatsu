b815e1500f705060c54d3a9f578267de4e60fda5
SebastianCh-M <80702362+SebastianCh-M@users.noreply.github.com>
2023-11-07 21:45:40 -0300

    cambios logout y login

--- Gatsu/views.py
+++ Gatsu/views.py
@@ -1,8 +1,8 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render
 from django.views import View
 from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
-from django.contrib.auth import logout
+
 
 class HomeView(View):
     def get(self, request, *args, **kwargs):
--- manga/urls.py
+++ manga/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import MangaListView, MangaCreateView, exit
+from .views import MangaListView, MangaCreateView
 
 app_name="manga"
 
