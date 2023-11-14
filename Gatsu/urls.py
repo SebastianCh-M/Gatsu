da5250fe0c5c9bc93aa23230b9e54a11a06c81cc
Felipe Bazares <fe.bazares@duocuc.cl>
2023-11-13 22:47:42 -0300

    pequeño commit

--- Gatsu/urls.py
+++ Gatsu/urls.py
@@ -8,12 +8,11 @@ from .views import HomeView, LoginView, RecientesView, TopMangasView, SobreGatsu
 
 urlpatterns = [
 
-    path('admin/', admin.site.urls),
+    path('admin', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
     path('', HomeView.as_view(), name = "home"),
     path('login.html', LoginView.as_view(), name="login"),
     path('Home', HomeView.as_view(), name='home'),
-<<<<<<< HEAD
     path('Recientes', RecientesView.as_view(), name='recientes'),
     path('TopMangas', TopMangasView.as_view(), name='TopMangas'),
     path('SobreGatsu', SobreGatsuView.as_view(), name='SobreGatsu'),
@@ -23,16 +22,6 @@ urlpatterns = [
     path('manga_create', registrarManga),
     path('manga_list', manga_list),
     path('capitulos_form',registrarImagenes)
-=======
-    path('Recientes.html', RecientesView.as_view(), name='recientes'),
-    path('TopMangas.html', TopMangasView.as_view(), name='TopMangas'),
-    path('SobreGatsu.html', SobreGatsuView.as_view(), name='SobreGatsu'),
-    path('MiBiblioteca.html', MiBibliotecaView.as_view(), name='MiBiblioteca'),
-    path('Recuperar.html', RecuperarView.as_view(), name='Recuperar'),
-    path('Home.html', HomeView.as_view(), name='home'),  # Ruta para la página por defecto
-    path('manga_create.html', registrarManga),
-    path('manga_list.html', manga_list),
->>>>>>> 3f0c3e9e3b892dea795a430e815d05c7044d56e9
 ] 
 
 
