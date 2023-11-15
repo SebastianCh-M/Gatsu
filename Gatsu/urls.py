4841151a8ceeefb8d760accf590463a4abe85073
Felipe Bazares <fe.bazares@duocuc.cl>
2023-11-15 19:02:54 -0300

    Arreglar_problemas_migrate

--- Gatsu/urls.py
+++ Gatsu/urls.py
@@ -3,13 +3,8 @@ from django.urls import path, include
 from django.http import HttpResponse
 from django.conf import settings
 from django.conf.urls.static import static
-<<<<<<< HEAD
 from manga.views import formRevista, listaRevista, deleR, updaR, formNombreManga, listaNombreManga, deleN, updaN, formMangaGatsu, listaMangaGatsu, deleM, updaM, formCapitulo, listaCapitulo, deleC, updaC, formImagen, listaImagen, deleI, updaI
-from .views import HomeView, LoginView, RecientesView, TopMangasView, SobreGatsuView, MiBibliotecaView, RecuperarView
-=======
-from manga.views import registrarManga, manga_list
 from .views import HomeView, RecientesView, TopMangasView, SobreGatsuView, MiBibliotecaView
->>>>>>> a1601ca3831f149ee44999475a20dc38e1ff4a9d
 
 urlpatterns = [
 
@@ -17,12 +12,10 @@ urlpatterns = [
     path('accounts/', include('django.contrib.auth.urls')),
     path('', HomeView.as_view(), name = "home"),
     path('Home', HomeView.as_view(), name='home'),
-<<<<<<< HEAD
     path('Recientes', RecientesView.as_view(), name='recientes'),
     path('TopMangas', TopMangasView.as_view(), name='TopMangas'),
     path('SobreGatsu', SobreGatsuView.as_view(), name='SobreGatsu'),
     path('MiBiblioteca', MiBibliotecaView.as_view(), name='MiBiblioteca'),
-    path('Recuperar', RecuperarView.as_view(), name='Recuperar'),
     path('', HomeView.as_view(), name='default'),  # Ruta para la página por defecto
 
     #Revisa
@@ -65,15 +58,6 @@ urlpatterns = [
     #path('manga_create', registrarManga),
     #path('manga_list', manga_list),
     #path('capitulos_form',registrarImagenes)
-=======
-    path('Recientes.html', RecientesView.as_view(), name='recientes'),
-    path('TopMangas.html', TopMangasView.as_view(), name='TopMangas'),
-    path('SobreGatsu.html', SobreGatsuView.as_view(), name='SobreGatsu'),
-    path('MiBiblioteca.html', MiBibliotecaView.as_view(), name='MiBiblioteca'),
-    path('Home.html', HomeView.as_view(), name='home'),  # Ruta para la página por defecto
-    path('manga_create.html', registrarManga),
-    path('manga_list.html', manga_list),
->>>>>>> a1601ca3831f149ee44999475a20dc38e1ff4a9d
 ] 
 
 
